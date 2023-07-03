4e14f42ff197dad8a7f13ec3fb033f06b8741a96
sdandu <dsraju15@gmail.com>
2023-07-04 00:37:33 +0530

    111

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,10 @@
-source common.sh
+#source common.sh
 
-print_head "Copy MongoDB Repo file"
+#print_head "Copy MongoDB Repo file"
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
 status_check
 
-print_head "Install MongoDB"
+#print_head "Install MongoDB"
 yum install mongodb-org -y  &>>${LOG}
 status_check
 
