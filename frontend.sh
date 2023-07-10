bc9fb5ffa1430d63a6059f6ac8e06277e2955728
sdandu <dsraju15@gmail.com>
2023-07-10 12:12:12 +0530

    111

--- frontend.sh
+++ frontend.sh
@@ -3,13 +3,21 @@ LOG=/tmp.roboshop.log
 
 echo -e '\e[35m Install Nginx\e[0m'
 yum install nginx -y &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failure
+fi
 
 systemctl enable nginx
 systemctl start nginx
 echo -e '\e[35m Remove Nginx old content\e[0m'
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failure
+fi
 
 echo -e '\e[35m Download Frontend content\e[0m'
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
@@ -17,12 +25,22 @@ echo $?
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failure
+
+fi
 
 echo -e '\e[35m Copy the Roboshot Contect\e[0m'
 cp ${script_location}/Files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failure
+
+fi
 
 #Trouble shooting commnds
 #sudo netstat -lntp
